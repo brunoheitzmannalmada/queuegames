9bfe5f6e4a0a59c94cc88943159dfe53f29a63f8
Bruno Heitzmann Almada <brunoalmada1@hotmail.com>
2016-05-15 13:22:43 +0200

    Renaming ProductRepository and GamesRepository

---
+++ QueueGames.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using QueueGames.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueGames.Data.Repositories
+{
+    public class ProductRepository : Base<Products>
+    {
+        
+    }
+}
