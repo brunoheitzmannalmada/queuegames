e574072184defe08c046d5746e058f830d6efba0
Bruno <Bruno@DESKTOP-CO2F9D4>
2017-03-04 14:36:08 -0300

    Setting HTML5 mode for Angular routing

--- QueueGames.AngularApp/app/app.config.js
+++ QueueGames.AngularApp/app/app.config.js
@@ -1,13 +1,17 @@
 ﻿app.config(function ($routeProvider, $locationProvider) {
-  //$locationProvider.html5Mode(true);
-
   $routeProvider
-      .when('/', {
+      .when('/Games', {
         templateUrl: '/app/components/games/GamesView.html',
         controller: 'GamesController'
       })
       .when('/Products', {
         templateUrl: '/app/components/products/ProductsView.html',
         controller: 'ProductsController'
+      })
+      .otherwise({
+        templateUrl: '/app/components/games/GamesView.html',
+        controller: 'GamesController'
       });
+
+    $locationProvider.html5Mode(true);
 });
